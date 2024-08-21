eed15d83929bb796e9a7f1aa581cd10f03df490a
Knowledge-JO <megamindtheincredible@gmail.com>
2024-08-21 14:00:00 +0100

    desktop only

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,15 +7,29 @@ import Supporters from "./Supporters"
 import Trial from "./Trial"
 function App() {
   return (
-    <div className="">
-      <Hero />
-      <Supporters />
-      <Features />
-      <Reviews />
-      <Faqs />
-      <Trial />
-      <Footer />
-    </div>
+    <>
+      <div className="xl:hidden min-h-screen flex justify-center items-center">
+        <div className="bg-[url('/src/assets/Background-pattern.png')] bg-no-repeat bg-center bg-cover">
+          <img
+            src="/mobile.webp"
+            alt=""
+            className="shadow-xl rounded-xl mb-3"
+          />
+          <p className="text-center text-gray-700 font-bold">
+            Please visit the site on a desktop
+          </p>
+        </div>
+      </div>
+      <div className="hidden xl:block">
+        <Hero />
+        <Supporters />
+        <Features />
+        <Reviews />
+        <Faqs />
+        <Trial />
+        <Footer />
+      </div>
+    </>
   )
 }
 
